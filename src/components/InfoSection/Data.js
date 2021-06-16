2e7ef1e8405f06e68e23e7dab7872c4a967d9d34
Rahul Kumar Singh <rahulkrsingh32@gmail.com>
2021-06-16 21:45:14 +0530

    pushed fresh codes

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObject = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions withoud getting charged any fdescribe.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary :true,
+    darkText: false
+};
+
+export const homeObjecttwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited access',
+    headline: 'Log in to your Account at any time.',
+    description: 'We have you coveres no matter where you are located. All you need is an internet connection and a phone or computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Money',
+    dark: false,
+    primary :true,
+    darkText: true
+};
+
+export const homeObjectthree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is Extemely easy',
+    description: "Get everything set Up and ready in under 10 min. All you need to do is add your information and you're ready to global.",
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-5.svg').default,
+    alt: 'papers',
+    dark: false,
+    primary :true,
+    darkText: true
+};
